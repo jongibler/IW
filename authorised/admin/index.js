4c4f1489b962cfb4566211aa28dd7d87dccc147e
Jonathan <jgibler@gmail.com>
2016-07-23 16:06:18 +0100

    progress.  ignore config

--- authorised/admin/index.js
+++ authorised/admin/index.js
@@ -1,8 +1,9 @@
-angular.module('admin', [])
+angular.module('admin',  ['ngTagsInput'])
     .controller('AdminController', function ($scope, $http, $location) {
 
-         $scope.allTalents = [{"_id":"57912d820fad1df40b1c3508","cvURL":"http://s3-eu-west-1.amazonaws.com/minimalnode/demo/1469132161937.pdf","email":"j@j.gom","name":"asdf","__v":0,"skills":["c#","javascript","html","nodejs"]}];
-
+         $scope.allTalents = [];
+        
+        $scope.talent = { name : "John", email: "a@acm", skills : [{text:'a'}]};
          
 
         $http.get('/api/talent')
@@ -12,4 +13,8 @@ angular.module('admin', [])
             .error(function (data) {
                 console.log('Error:' + data);
             });
+
+
     });
+
+
